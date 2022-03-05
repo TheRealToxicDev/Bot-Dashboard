25b263dc0d566307bc66748b222c0496f02c134a
Diabla <95551108+ddiablou@users.noreply.github.com>
2022-03-05 15:59:38 +0100

    Dynamic alt prop

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,15 +3,16 @@ interface Props {
   link: string;
   width: number;
   height: number;
+  alt?: string;
   className?: string;
 }
 import Image from "next/image";
 
-export function Avatar({ link, width, height, className }: Props) {
+export function Avatar({ link, width, height, alt, className }: Props) {
   return (
     <Image
       src={`${link}`}
-      alt="Logo"
+      alt={alt || "Logo"}
       width={width}
       height={height}
       className={className || "w-8 rounded-full"}
